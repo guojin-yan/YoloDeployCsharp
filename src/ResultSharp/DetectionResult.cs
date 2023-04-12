4366a5bc95d71b1bc660217b2d6bb9b6acf95b03
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-04-12 17:33:12 +0800

    建立结果处理类

---
+++ src/ResultSharp/DetectionResult.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+
+namespace ResultSharp
+{
+    public class DetectionResult: ResultBase
+    {
+        public DetectionResult(string path, float[] factors, float score_threshold=0.25f, float nms_threshold=0.45f) {
+            read_class_names(path);
+            this.factors = factors;
+            this.score_threshold = score_threshold;
+            this.nms_threshold = nms_threshold;
+        }
+
+        public Result process_resule(float[] result)
+        {
+            Mat result_data = new Mat(84, 8400, MatType.CV_32F, result);
+            result_data = result_data.T();
+
+            // 存放结果list
+            List<Rect> position_boxes = new List<Rect>();
+            List<int> class_ids = new List<int>();
+            List<float> confidences = new List<float>();
+            // 预处理输出结果
+            for (int i = 0; i < result_data.Rows; i++)
+            {
+                // 获取置信值
+                //float confidence = result_data.At<float>(i, 4);
+                //if (confidence < this.score_threshold)
+                //{
+                //    continue;
+                //}
+                //Console.WriteLine(confidence);
+
+                Mat classes_scores = result_data.Row(i).ColRange(4, 84);//GetArray(i, 5, classes_scores);
+                Point max_classId_point, min_classId_point;
+                double max_score, min_score;
+                // 获取一组数据中最大值及其位置
+                Cv2.MinMaxLoc(classes_scores, out min_score, out max_score,
+                    out min_classId_point, out max_classId_point);
+                // 置信度 0～1之间
+                // 获取识别框信息
+                if (max_score > 0.25)
+                {
+                    Console.WriteLine(max_score);
+                    float cx = result_data.At<float>(i, 0);
+                    float cy = result_data.At<float>(i, 1);
+                    float ow = result_data.At<float>(i, 2);
+                    float oh = result_data.At<float>(i, 3);
+                    int x = (int)((cx - 0.5 * ow) * this.factors[0]);
+                    int y = (int)((cy - 0.5 * oh) * this.factors[1]);
+                    int width = (int)(ow * this.factors[0]);
+                    int height = (int)(oh * this.factors[1]);
+                    Rect box = new Rect();
+                    box.X = x;
+                    box.Y = y;
+                    box.Width = width;
+                    box.Height = height;
+
+                    position_boxes.Add(box);
+                    class_ids.Add(max_classId_point.X);
+                    confidences.Add((float)max_score);
+                }
+            }
+
+            // NMS非极大值抑制
+            int[] indexes = new int[position_boxes.Count];
+            CvDnn.NMSBoxes(position_boxes, confidences, this.score_threshold, this.nms_threshold, out indexes);
+
+            Result re_result= new Result();
+            // 将识别结果绘制到图片上
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                int index = indexes[i];
+                int idx = class_ids[index];
+                re_result.add(confidences[index], position_boxes[index], this.class_names[class_ids[index]]);
+            }
+            return re_result;
+        }
+        public Mat draw_result(Result result, Mat image) {
+
+            // 将识别结果绘制到图片上
+            for (int i = 0; i < result.length; i++)
+            {
+                Cv2.Rectangle(image, result.rects[i], new Scalar(0, 0, 255), 2, LineTypes.Link8);
+                Cv2.Rectangle(image, new Point(result.rects[i].TopLeft.X, result.rects[i].TopLeft.Y - 20),
+                    new Point(result.rects[i].BottomRight.X, result.rects[i].TopLeft.Y), new Scalar(0, 255, 255), -1);
+                Cv2.PutText(image, result.classes[i] + "-" + result.scores[i].ToString("0.00"),
+                    new Point(result.rects[i].X, result.rects[i].Y - 10),
+                    HersheyFonts.HersheySimplex, 0.6, new Scalar(0, 0, 0), 1);
+            }
+            return image;
+        }
+
+    }
+}
---
+++ src/ResultSharp/ResultBase.cs
@@ -0,0 +1,37 @@
+﻿namespace ResultSharp
+{
+    public class ResultBase
+    {
+        // 识别结果类型
+        public string[] class_names;
+        // 图片放缩比例
+        public float[] factors;
+        // 置信度阈值
+        public float score_threshold;
+        // 非极大值抑制阈值
+        public float nms_threshold;
+
+
+        public ResultBase() { }
+
+
+        /// <summary>
+        /// 读取本地识别结果类型文件到内存
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        public void read_class_names(string path)
+        {
+
+            List<string> str = new List<string>();
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                str.Add(line);
+            }
+
+            class_names = str.ToArray();
+        }
+
+    }
+}
