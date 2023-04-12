753a7bc9bbd96d0d34b1ee3730e14967e7ea7ff2
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-04-12 17:32:59 +0800

    建立结果类

---
+++ src/ResultSharp/Result.cs
@@ -0,0 +1,33 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultSharp
+{
+    public class Result
+    {
+        // 获取结果长度
+        public int length 
+        { get 
+            { 
+                return scores.Count;
+            } 
+        }
+
+        // 识别结果类
+        public List<string> classes = new List<string>();
+        // 置信值
+        public List<float> scores = new List<float>();
+        // 预测框
+        public List<Rect> rects = new List<Rect>();
+
+        public void add(float score, Rect rect, string cla) {
+            scores.Add(score);
+            rects.Add(rect);
+            classes.Add(cla);
+        }
+    }
+}
