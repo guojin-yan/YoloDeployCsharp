3f87d4fbb914b8acaacbe9af79333f1430426621
yanguojin <guojin_yjs@cumt.edu.cn>
2024-05-03 21:03:05 +0800

    Add Log class.

---
+++ src/YoloDeployPlatform/log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoloDeployPlatform
+{
+    public class Log
+    {
+        public bool flag_time {get;set;}
+        public bool flag_fps { get; set; }
+
+        public long infer_time { get; set; }
+        private TextBox current_cb;
+
+        private static readonly Log instance = new Log();
+        private Log() { }
+
+        public static Log Instance
+        {
+            get { return instance; }
+        }
+
+        
+        public void set_current_cb(TextBox cb) 
+        {
+            current_cb = cb;
+        }
+
+        public void print(string msg) 
+        {
+            current_cb.AppendText(msg + "\r\n");
+        }
+    }
+}
