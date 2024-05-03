3fc2a76a4bde0074f65596e6996897dec1ec1fa8
yanguojin <guojin_yjs@cumt.edu.cn>
2024-05-03 21:04:11 +0800

    Add support for the YOLOv5-Det model.

---
+++ src/YoloDeployPlatform/predictor/yolov5-det.cs
@@ -0,0 +1,78 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using OpenVinoSharp.Extensions.result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Compilation;
+
+namespace YoloDeployPlatform.predictor
+{
+    public class YOLOv5Det : YOLO
+    {
+        public YOLOv5Det(string model_path, EngineType engine, string device, int categ_nums, float det_thresh, float det_nms_thresh, int input_size)
+            : base(model_path, engine, device, categ_nums, det_thresh, det_nms_thresh, new int[] { 1, 3, input_size, input_size }, 
+                  new List<string> { "images" }, new List<int[]> { new int[] { 1, 25200, 5 + categ_nums } }, new List<string> { "output" })
+        {
+        }
+        protected override BaseResult postprocess(List<float[]> results)
+        {
+            Mat result_data = new Mat(this.m_output_sizes[0][1], this.m_output_sizes[0][2], MatType.CV_32F, results[0]);
+
+            // Storage results list
+            List<Rect> position_boxes = new List<Rect>();
+            List<int> class_ids = new List<int>();
+            List<float> confidences = new List<float>();
+            // Preprocessing output results
+            for (int i = 0; i < result_data.Rows; i++)
+            {
+                float confidence = result_data.At<float>(i, 4);
+                if (confidence < 0.25)
+                {
+                    continue;
+                }
+                Mat classes_scores = new Mat(result_data, new Rect(5, i, m_categ_nums, 1));
+                OpenCvSharp.Point max_classId_point, min_classId_point;
+                double max_score, min_score;
+                // Obtain the maximum value and its position in a set of data
+                Cv2.MinMaxLoc(classes_scores, out min_score, out max_score,
+                    out min_classId_point, out max_classId_point);
+                // Confidence level between 0 ~ 1
+                // Obtain identification box information
+                if (max_score > this.m_det_thresh)
+                {
+                    float cx = result_data.At<float>(i, 0);
+                    float cy = result_data.At<float>(i, 1);
+                    float ow = result_data.At<float>(i, 2);
+                    float oh = result_data.At<float>(i, 3);
+                    int x = (int)((cx - 0.5 * ow) * this.m_factor);
+                    int y = (int)((cy - 0.5 * oh) * m_factor);
+                    int width = (int)(ow * m_factor);
+                    int height = (int)(oh * m_factor);
+                    Rect box = new Rect();
+                    box.X = x;
+                    box.Y = y;
+                    box.Width = width;
+                    box.Height = height;
+
+                    position_boxes.Add(box);
+                    class_ids.Add(max_classId_point.X);
+                    confidences.Add((float)confidence);
+                }
+            }
+            // NMS non maximum suppression
+            int[] indexes = new int[position_boxes.Count];
+            CvDnn.NMSBoxes(position_boxes, confidences, this.m_det_thresh, this.m_det_nms_thresh, out indexes);
+            DetResult re = new DetResult();
+            // 
+            for (int i = 0; i < indexes.Length; i++)
+            {
+                int index = indexes[i];
+                re.add(class_ids[index], confidences[index], position_boxes[index]);
+            }
+            return re;
+        }
+    }
+}
